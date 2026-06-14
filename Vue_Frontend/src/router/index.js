275447397492240b407b727823940a178a41a7e9
Leo Andrighetto <leoandrighetto02@gmail.com>
2026-06-14 14:39:01 -0300

    @feat - criação da registerView
    e  sua rota.

--- Vue_Frontend/src/router/index.js
+++ Vue_Frontend/src/router/index.js
@@ -1,13 +1,16 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import HomeView from "../pages/HomeView.vue";
+import RegisterView from "../pages/authentication/RegisterView.vue";
 
 const routes = [
-    { path: "/", component: HomeView }
+  { path: "/", component: HomeView },
+  { path: "/register", component: RegisterView },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes});
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
