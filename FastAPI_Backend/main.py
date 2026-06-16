3d86ec2b62b62c7b9005385639259a3c0a3b43dd
Leo Andrighetto <leoandrighetto02@gmail.com>
2026-06-15 23:48:36 -0300

    @feat consluído

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from database import engine, get_session
 from sqlmodel import SQLModel
 
 # from models.model import model
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    SQLModel.metadata.create_all(engine) 
+    SQLModel.metadata.create_all(engine)
     yield
     print("Desligando a aplicação...")
 
+
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:5173/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
